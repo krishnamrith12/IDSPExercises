15fd79f7b95a145d96fe891f518ff2da62bcf139
Amrith <krishnamrith12@gmail.com>
2019-09-23 09:32:31 +0200

    moved

--- errorFiles/errorFiles4.py
+++
@@ -1,6 +0,0 @@
-
-def printList(theList):
-    print("the List is",theList)
-
-aList = [1,2,3,4]
-printList(aList.append(5))
--- errorFiles/errorFiles5.py
+++
@@ -1,7 +0,0 @@
-row = [""]*3
-board = [row]*3
-print(board)
-print(board[0])
-print(board[0][0])
-board[0][0] = "X"
-print(board)
